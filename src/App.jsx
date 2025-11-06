3ed2b7c61cf42eb503a970e199848dd35643ab4b
Jose Ochoa <joseochoa@Mac.lan>
2025-11-06 17:14:49 -0500

    Created a Header component and a Footer component. Included them in the main app component. Couldn't find a good footer in bootstrap so wrote custom css code for it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import EmployeeListComponent from './components/EmployeeListComponent'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderComponent/>
       <EmployeeListComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className="footer">
+                <span>This project was made by Jose G. Ochoa Rios</span>
+            </footer>
+
+        </div>
+    )
+}
+
+
+export default FooterComponent

a32fce0fffe112d24423137d9518f6f9fedec991
Jose Ochoa <joseochoa@Mac.lan>
2025-11-06 18:28:20 -0500

    Added React Router to app. React router allows to  add navigation to the React app.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import './App.css'
 import EmployeeListComponent from './components/EmployeeListComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent/>
-      <EmployeeListComponent />
-      <FooterComponent />
+      {/* <BrowserRouter> - enables routing, listens to browser’s URL and decides which component to show. */}
+      <BrowserRouter>
+        <HeaderComponent/>
+        {/* <Routes> - holds all different routes */}
+        <Routes>
+          {/* Route> - defines one “path” and the component to display for it. */}
+          <Route path='/' element= { <EmployeeListComponent /> } />
+          <Route path='/employees' element={ <EmployeeListComponent /> } />
+
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }

1e34bd724a907c28def1b68359a42b0f65193192
Jose Ochoa <joseochoa@Mac.lan>
2025-11-06 12:13:18 -0500

    Added bootstrap to project.

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HelloWorld() {
     return(
         <>
-            <h1>Hello World!</h1>
+            <h1 className="text-center">Hello World!</h1>
         </>
     );
 

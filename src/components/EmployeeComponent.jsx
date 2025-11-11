a5136d1f93eb6b697a40d11e851555228b1873d1
Jose Ochoa <joseochoa@Mac.lan>
2025-11-11 16:16:21 -0500

    Changed EmployeeComponent to have an actual form with fields and added  Employee Form Handling.

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+
+    // initializing variables to empty strings”
+    // firstName — the current value 
+    // setFirstName — function we can call to update that value
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+
+    // we can create arrow functions to be called onClick.
+    // or we can pass the arrow function directly in  the onClick = {}
+    const firstNameHandler = (e) => { setFirstName(e.target.value); }
+    const lastNameHandler = (e) => { setLastName(e.target.value); }
+    const emailHandler = (e) => { setEmail(e.target.value); }
+
+    //
+    function saveEmployee (e) {
+        // prevents browser from doing its default action of page refresh or navigate away.
+        e.preventDefault();
+
+        const employee = {firstName, lastName, email}
+        console.log(employee);
+    }
+
   return (
-    <div>
-      EmployeeComponent
+    <div className='container'>
+        <br /><br />
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3' >
+                <h2 className='text-center'>Add Employee</h2>
+                <div className='card-body'>
+                    <form>
+                        {/* first name label and input */}
+                        <div className='form-group mb-2' >
+                            <label className='form-label'>First Name:</label>
+                            <input 
+                                type='text'
+                                placeholder='Enter Employee First Name'
+                                name='firstName'
+                                value={firstName}
+                                className='form-control'
+                                // if the arrow function is only 1 line we can pass it directly.
+                                onChange={ (e) => setFirstName(e.target.value) }
+                            ></input>
+                        </div>
+                        {/* last name label and input */}
+                        <div className='form-group mb-2' >
+                            <label className='form-label'>Last Name:</label>
+                            <input 
+                                type='text'
+                                placeholder='Enter Employee Last Name'
+                                name='lastName'
+                                value={lastName}
+                                className='form-control'
+                                onChange={ (e) => setLastName(e.target.value) }
+                            ></input>
+                        </div>
+                        {/* email label and input */}
+                        <div className='form-group mb-2' >
+                            <label className='form-label'>Email:</label>
+                            <input 
+                                type='text'
+                                placeholder='Enter Employee Email'
+                                name='email'
+                                value={email}
+                                className='form-control'
+                                onChange={ (e) => setEmail(e.target.value) }
+                            ></input>
+                        </div>
+                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }

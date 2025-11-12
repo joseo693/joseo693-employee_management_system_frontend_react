6eafc85eb51b9896d8ae3522d4f3d3fcc991327c
Jose Ochoa <joseochoa@Mac.lan>
2025-11-12 17:15:51 -0500

    Added Form validation to the EmployeeComponent.

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -11,31 +11,79 @@ const EmployeeComponent = () => {
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
 
+    const [errors, setErrors] = useState( { 
+        firstName: '',
+        lastName: '',
+        email: ''
+    } )
+
     // we can create arrow functions to be called onClick.
     // or we can pass the arrow function directly in  the onClick = {}
     const firstNameHandler = (e) => { setFirstName(e.target.value); }
     const lastNameHandler = (e) => { setLastName(e.target.value); }
     const emailHandler = (e) => { setEmail(e.target.value); }
 
-    //
     const navigator = useNavigate();
 
-    //
+    // gets called when the submit button is clicked to create a new employee
     function saveEmployee (e) {
-        // prevents browser from doing its default action of page refresh or navigate away.
+        // prevents browser from doing its default action of page refresh or navigate away
         e.preventDefault();
 
-        const employee = {firstName, lastName, email}
-        console.log(employee);
+        // if validateForm() = true, create Employee
+        if( validateForm() ){
 
-        // calling createEmployee() from EmployeeService.js
-        createEmployee(employee).then( (response) => {
-            console.log(response.data);
+            const employee = {firstName, lastName, email}
+            console.log(employee);
 
-            // navigates back to the list of employees
-            navigator('/employees')
-        } )
+            // calling createEmployee() from EmployeeService.js
+            createEmployee(employee).then( (response) => {
+                console.log(response.data);
+
+                // navigates back to the list of employees
+                navigator('/employees')
+            } )
+
+        }
     }
+
+    // Validating form data
+    function validateForm () {
+        let valid = true;
+
+        // create a copy of current errors object
+        // spread operator clones object so it can safely be modified  without changing the original directly
+        const errorsCopy = {... errors}
+
+        // Check if the field isn’t empty
+        if(firstName.trim()) {
+            errorsCopy.firstName = '';
+        } else {
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+        if(lastName.trim()) {
+            errorsCopy.lastName = '';
+        } else {
+            errorsCopy.lastName = 'Last name is required';
+            valid = false;
+        }
+
+        if(email.trim()) {
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+
+        // updates state of errors object with the new error messages
+        setErrors(errorsCopy);
+
+        // returns a simple true/false flag to tell submit function whether the form passed validation
+        return valid;
+    }
+
 
   return (
     <div className='container'>
@@ -53,10 +101,13 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee First Name'
                                 name='firstName'
                                 value={firstName}
-                                className='form-control'
+                                // controls the CSS styling dynamically based on whether there’s an error
+                                className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
                                 // if the arrow function is only 1 line we can pass it directly.
                                 onChange={ (e) => setFirstName(e.target.value) }
                             ></input>
+                            {/* conditional rendering — only shows <div> if there’s an error message */}
+                            { errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div> }
                         </div>
                         {/* last name label and input */}
                         <div className='form-group mb-2' >
@@ -66,9 +117,10 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee Last Name'
                                 name='lastName'
                                 value={lastName}
-                                className='form-control'
+                                className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
                                 onChange={ (e) => setLastName(e.target.value) }
                             ></input>
+                            { errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div> }
                         </div>
                         {/* email label and input */}
                         <div className='form-group mb-2' >
@@ -78,9 +130,10 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee Email'
                                 name='email'
                                 value={email}
-                                className='form-control'
+                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
                                 onChange={ (e) => setEmail(e.target.value) }
                             ></input>
+                            { errors.email && <div className='invalid-feedback'>{errors.email}</div> }
                         </div>
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
 
